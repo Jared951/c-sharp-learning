8a589f85b9d0f22965a903c5e4c29fe789fb3ad1
Jared Sewak <jaredsewak@gmail.com>
2023-10-02 20:11:55 -0600

    if else statements

--- basics.cs
+++ basics.cs
@@ -324,17 +324,100 @@ Stateless methods can be used directly.
 Stateful methods require creating an object (instance) of the class using the new operator.
 */
 
-Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+Random diceRoll = new Random();
+int rollOne = diceRoll.Next();
+int rollTwo = diceRoll.Next(101);
+int rollThree = diceRoll.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+Console.WriteLine($"First roll: {rollOne}");
+Console.WriteLine($"Second roll: {rollTwo}");
+Console.WriteLine($"Third roll: {rollThree}");
 
 int firstValue = 500;
 int secondValue = 600;
 int largerValue= Math.Max(firstValue, secondValue); 
 
 Console.WriteLine(largerValue);
+
+
+
+
+
+
+
+// IF ELSE STATEMENTS
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonues to total!");
+        total += 2;
+    }
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+
+
+
+
+
+// IF ELSE CHALLENGE
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
